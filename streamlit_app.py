2aa977bc3aed0b05dcd4d4ad1d484c217db87431
Capsy2111 <165657487+Capsy2111@users.noreply.github.com>
2024-04-02 14:33:16 +0530

    Update streamlit_app.py
    added nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if ingredients_list :
     
     for fruits_choosen in ingredients_list:
         ingredients_string += fruits_choosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruits_choosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruits_choosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
