a3b05d12091c3e72688f3f96ae6c4c5d8b2b4313
Capsy2111 <165657487+Capsy2111@users.noreply.github.com>
2024-04-02 10:20:13 +0530

    Update streamlit_app.py
    replaces col line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -14,7 +15,6 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name of your smoothie will be: ', name_on_order)
 
-from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
